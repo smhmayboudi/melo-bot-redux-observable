d0ece552e0f8827d3450d50f0081a7c7d88eb208
smhmayboudi <smhmayboudi@gmail.com>
2019-10-20 00:16:49 +0330

    Update test cases

--- src/middlewares/crashReporter.test.ts
+++ src/middlewares/crashReporter.test.ts
@@ -1,9 +1,67 @@
 import { Action } from "redux";
 
+import { IStateMessageQuery } from "../../types/iStateMessageQuery";
+import * as actions from "../actions";
+
 import * as middleware from "./crashReporter";
 
 describe("crashReporter middleware", (): void => {
+  const query: IStateMessageQuery = {
+    message: {
+      chat: {
+        first_name: "Hossein",
+        id: 52953379,
+        last_name: "Mayboudi",
+        type: "private",
+        username: "smhmayboudi"
+      },
+      date: 1537627954,
+      entities: [
+        {
+          length: 9,
+          offset: 0,
+          type: "bot_command"
+        }
+      ],
+      from: {
+        first_name: "Hossein",
+        id: 52953379,
+        is_bot: false,
+        language_code: "fa",
+        last_name: "Mayboudi",
+        username: "smhmayboudi"
+      },
+      message_id: 1164,
+      text: ""
+    },
+    update_id: 0
+  };
+  const queryFrom: IStateMessageQuery = {
+    message: {
+      chat: {
+        first_name: "Hossein",
+        id: 52953379,
+        last_name: "Mayboudi",
+        type: "private",
+        username: "smhmayboudi"
+      },
+      date: 1537627954,
+      entities: [
+        {
+          length: 9,
+          offset: 0,
+          type: "bot_command"
+        }
+      ],
+      from: undefined,
+      message_id: 1164,
+      text: ""
+    },
+    update_id: 0
+  };
+
   const create: (
+    getState: jest.Mock,
     error?: () => void
   ) => {
     next: jest.Mock;
@@ -13,6 +71,7 @@ describe("crashReporter middleware", (): void => {
     };
     invoke(action: Action<string>): Action<string>;
   } = (
+    getState: jest.Mock,
     error?: () => void
   ): {
     next: jest.Mock;
@@ -27,7 +86,7 @@ describe("crashReporter middleware", (): void => {
       getState: jest.Mock;
     } = {
       dispatch: jest.fn(),
-      getState: jest.fn(() => ({}))
+      getState
     };
     const next: jest.Mock = jest.fn(error);
     const invoke: (action: Action<string>) => Action<string> = (
@@ -38,17 +97,49 @@ describe("crashReporter middleware", (): void => {
   };
 
   test("should handle", (): void => {
-    const { invoke, next } = create();
+    const { invoke, next } = create(jest.fn(() => ({})));
     const action: Action<string> = { type: "" };
     invoke(action);
     expect(next).toHaveBeenCalledWith(action);
   });
 
-  test("should handle exception", (): void => {
+  test("should handle exception action", (): void => {
     const error: Error = new Error("");
-    const { next, invoke } = create((): void => {
+    const { next, invoke } = create(jest.fn(() => ({})), (): void => {
       throw error;
     });
+    const action: Action<string> = actions.message.query({ query });
+    try {
+      invoke(action);
+    } catch {
+      expect(next).toThrow(error);
+    }
+  });
+
+  test("should handle exception state from undefined", (): void => {
+    const error: Error = new Error("");
+    const { next, invoke } = create(
+      jest.fn(() => ({ message: { queryFrom } })),
+      (): void => {
+        throw error;
+      }
+    );
+    const action: Action<string> = { type: "" };
+    try {
+      invoke(action);
+    } catch {
+      expect(next).toThrow(error);
+    }
+  });
+
+  test("should handle exception state", (): void => {
+    const error: Error = new Error("");
+    const { next, invoke } = create(
+      jest.fn(() => ({ message: { query } })),
+      (): void => {
+        throw error;
+      }
+    );
     const action: Action<string> = { type: "" };
     try {
       invoke(action);
