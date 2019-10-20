260d73a98957747d3987927252dfd1a5017ccad5
smhmayboudi <smhmayboudi@gmail.com>
2019-10-20 11:17:47 +0330

    Remove tests

--- src/config/env.test.ts
+++
@@ -1,7 +0,0 @@
-// Import * as env from "./env";
-
-// Describe("env config", (): void => {
-//     Test("should handle number", (): void => {});
-
-//     Test("should handle string", (): void => {});
-// });
