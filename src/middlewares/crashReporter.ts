923fd83df811e92c62e0d16c611ceb570f35e460
smhmayboudi <smhmayboudi@gmail.com>
2019-10-20 16:23:36 +0330

    Fix crashReport issues

--- src/middlewares/crashReporter.ts
+++ src/middlewares/crashReporter.ts
@@ -1,4 +1,13 @@
-import { captureException, configureScope, init, Scope } from "@sentry/node";
+import {
+  captureException,
+  configureScope,
+  getCurrentHub,
+  Hub,
+  init,
+  Scope,
+  Severity
+} from "@sentry/node";
+import { Integration } from "@sentry/types";
 import debug from "debug";
 import { AnyAction, Dispatch, Middleware, MiddlewareAPI } from "redux";
 
@@ -11,10 +20,22 @@ const appDebug: debug.IDebugger = debug("app:middleware:crashReport");
 
 init({
   dsn: env.SENTRY_DSN,
+  integrations: (integrations: Integration[]): Integration[] =>
+    integrations.filter(
+      (integration: Integration) => integration.name !== "OnUncaughtException"
+    ),
   onFatalError: (error: Error): void => {
     appDebug(error);
   },
   serverName: env.SENTRY_SERVERNAME
+});
+
+global.process.on("uncaughtException", (error: Error): void => {
+  const hub: Hub = getCurrentHub();
+  hub.withScope((scope: Scope): void => {
+    scope.setLevel(Severity.Fatal);
+    hub.captureException(error);
+  });
 });
 
 const crashReporter: Middleware = (
@@ -22,65 +43,67 @@ const crashReporter: Middleware = (
 ): ((next: Dispatch) => (action: AnyAction) => AnyAction) => (
   next: Dispatch
 ): ((action: AnyAction) => AnyAction) => (action: AnyAction): AnyAction => {
+  let message: IMessage = {
+    chat: {
+      id: 0,
+      type: "private"
+    },
+    date: 0,
+    message_id: 0
+  };
+  if (
+    action.type === actions.message.MESSAGE_QUERY &&
+    action.message !== undefined &&
+    action.message.query !== undefined &&
+    action.message.query.message !== undefined
+  ) {
+    message = action.message.query.message;
+  }
+  const state: IState = middlewareAPI.getState();
+  if (
+    state.message.query !== undefined &&
+    state.message.query.message !== undefined
+  ) {
+    message = state.message.query.message;
+  }
+  let firstName: string | undefined;
+  let id: number;
+  let isBot: boolean;
+  let languageCode: string | undefined;
+  let lastName: string | undefined;
+  let userName: string | undefined;
+  if (message.from !== undefined) {
+    firstName = message.from.first_name;
+    id = message.from.id;
+    isBot = message.from.is_bot;
+    languageCode = message.from.language_code;
+    lastName = message.from.last_name;
+    userName = message.from.username;
+  }
+
+  configureScope((scope: Scope) => {
+    scope.setExtras({
+      first_name: firstName,
+      is_bot: isBot,
+      language_code: languageCode,
+      last_name: lastName,
+      state
+    });
+    scope.setTags({
+      is_bot: `${isBot}`,
+      language_code: `${languageCode}`
+    });
+    scope.setUser({
+      email: `${id}@${userName}`,
+      id: `${id}`,
+      ip_address: undefined,
+      username: userName
+    });
+  });
   try {
     return next(action);
   } catch (error) {
     appDebug("exception", error);
-    let message: IMessage = {
-      chat: {
-        id: 0,
-        type: "private"
-      },
-      date: 0,
-      message_id: 0
-    };
-    if (
-      action.type === actions.message.MESSAGE_QUERY &&
-      action.message !== undefined &&
-      action.message.query !== undefined &&
-      action.message.query.message !== undefined
-    ) {
-      message = action.message.query.message;
-    }
-    const state: IState = middlewareAPI.getState();
-    if (
-      state.message.query !== undefined &&
-      state.message.query.message !== undefined
-    ) {
-      message = state.message.query.message;
-    }
-    let firstName: string | undefined;
-    let id: number;
-    let isBot: boolean;
-    let languageCode: string | undefined;
-    let lastName: string | undefined;
-    let userName: string | undefined;
-    if (message.from !== undefined) {
-      firstName = message.from.first_name;
-      id = message.from.id;
-      isBot = message.from.is_bot;
-      languageCode = message.from.language_code;
-      lastName = message.from.last_name;
-      userName = message.from.username;
-    }
-    configureScope((scope: Scope) => {
-      scope.setExtras({
-        first_name: firstName,
-        is_bot: isBot,
-        language_code: languageCode,
-        last_name: lastName
-      });
-      scope.setTags({
-        is_bot: `${isBot}`,
-        language_code: `${languageCode}`
-      });
-      scope.setUser({
-        email: undefined,
-        id: `${id}`,
-        ip_address: undefined,
-        username: userName
-      });
-    });
     captureException(error);
     throw error;
   }
