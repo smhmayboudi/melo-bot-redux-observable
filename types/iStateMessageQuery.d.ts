121b0efdfda7632afe103cbc39f53b15972e1e89
smhmayboudi <smhmayboudi@gmail.com>
2019-10-22 13:40:33 +0330

    Update type

--- types/iStateMessageQuery.d.ts
+++ types/iStateMessageQuery.d.ts
@@ -8,11 +8,20 @@ import { IShippingQuery } from "./telegramBot/payments/iShippingQuery";
 
 export interface IStateMessageQuery {
   callback_query?: ICallbackQuery;
+  channel_post?: IMessage;
   chosen_inline_result?: IChosenInlineResult;
+  edited_channel_post?: IMessage;
+  // Edited_channel_post_text?: IMessage;
+  // Edited_channel_post_caption?: IMessage;
+  edited_message?: IMessage;
+  // Edited_message_text?: IMessage;
+  // Edited_message_caption?: IMessage;
   error?: Error;
   inline_query?: IInlineQuery;
   message?: IMessage;
+  // Polling_error?: Error;
   pre_checkout_query?: IPreCheckoutQuery;
   shipping_query?: IShippingQuery;
   update_id: number;
+  // Webhook_error?: Error;
 }
