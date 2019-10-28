4e1964154e746a1d661d33595e26cdf4490cad53
smhmayboudi <smhmayboudi@gmail.com>
2019-10-28 09:44:13 +0330

    Fix bug

--- types/telegramBot/types/iChatPermissions.d.ts
+++ types/telegramBot/types/iChatPermissions.d.ts
@@ -1,6 +1,12 @@
 import { IChatPermissions } from "../types/iChatPermissions";
 
 export interface IChatPermissions {
-  chat_id: number | string,
-  permissions: IChatPermissions,
+  can_add_web_page_previews?: boolean;
+  can_change_info?: boolean;
+  can_invite_users?: boolean;
+  can_pin_messages?: boolean;
+  can_send_media_messages?: boolean;
+  can_send_messages?: boolean;
+  can_send_other_messages?: boolean;
+  can_send_polls?: boolean;
 }
