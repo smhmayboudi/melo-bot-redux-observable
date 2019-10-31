9dc44c00cfde68c5e8e030fd4ef7e1d33a957d09
smhmayboudi <smhmayboudi@gmail.com>
2019-10-31 12:49:41 +0330

    Fix type

--- types/telegramBot/inlineMode/iInlineQueryResultArticle.d.ts
+++ types/telegramBot/inlineMode/iInlineQueryResultArticle.d.ts
@@ -4,7 +4,7 @@ import { IInputMessageContent } from "./iInputMessageContent";
 export interface IInlineQueryResultArticle extends IInlineQueryResult {
   description?: string;
   hide_url?: boolean;
-  input_message_content?: IInputMessageContent;
+  input_message_content: IInputMessageContent;
   thumb_height?: number;
   thumb_url?: string;
   thumb_width?: number;
