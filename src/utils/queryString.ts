799134ec991dcc52d923fdd91bc43d4033a6ff60
smhmayboudi <smhmayboudi@gmail.com>
2019-11-03 13:23:16 +0330

    Update query string

--- src/utils/queryString.ts
+++ src/utils/queryString.ts
@@ -1,10 +1,10 @@
 import * as querystring from "querystring";
 import { IQueryString } from "../../types/iQueryString";
 
-const parse: (x: string) => IQueryString = (str: string): IQueryString =>
-  querystring.parse<IQueryString>(str, "-", "_");
+const parse: (str: string) => IQueryString = (str: string): IQueryString =>
+  querystring.parse<IQueryString>(str, "&", "=");
 
 const stringify: (obj: IQueryString) => string = (obj: IQueryString): string =>
-  querystring.stringify<IQueryString>(obj, "-", "_");
+  querystring.stringify<IQueryString>(obj, "&", "=");
 
 export { parse, stringify };
--- types/iQueryString.d.ts
+++ types/iQueryString.d.ts
@@ -1,7 +1,8 @@
 export interface IQueryString {
   c?: string;
-  pirpp: number;
-  pitr: number;
+  pirpp?: number;
+  pitr?: number;
   pt: string;
   q?: string;
+  [key: string]: any;
 }
